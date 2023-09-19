31ab7ebf3e273a66f2f8ea93938fc7bb0eeaedcc
Jack Goodby <jackgoodby@gmail.com>
2023-09-19 16:31:16 +0100

    Added dynamo struct tags

--- lambda/model/fixture.go
+++ lambda/model/fixture.go
@@ -1,26 +1,30 @@
 package model
 
 import (
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
-import "github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 
 type Fixture struct {
-	Id             string `json:"id"`
-	SortKey        string `json:"sort_key"`
-	StartAt        string `json:"start_at"`
-	EndedAt        string `json:"ended_at"`
-	Court          int64  `json:"court"`
-	InProgress     bool   `json:"in_progress"`
-	TournamentName string `json:"tournament_name"`
+	Id             string `json:"json_id" dynamodbav:"id"`
+	SortKey        string `json:"json_sort_key" dynamodbav:"sort_key"`
+	StartAt        string `json:"json_start_at" dynamodbav:"start_at,omitempty"`
+	EndedAt        string `json:"json_ended_at" dynamodbav:"ended_at,omitempty"`
+	Court          int64  `json:"json_court" dynamodbav:"court,omitempty"`
+	InProgress     bool   `json:"json_in_progress" dynamodbav:"in_progress,omitempty"`
+	TournamentName string `json:"json_tournament_name" dynamodbav:"tournament_name,omitempty"`
 }
 
-func (fixture Fixture) GetKey() map[string]types.AttributeValue {
-	id, err := attributevalue.Marshal(fixture.Id)
+func (f Fixture) GetTableName() string {
+	return "fixture" //TODO: do this as a public constant and whatnot
+}
+
+func (f Fixture) GetKey() map[string]types.AttributeValue {
+	id, err := attributevalue.Marshal(f.Id)
 	if err != nil {
 		panic(err)
 	}
-	sk, err := attributevalue.Marshal(fixture.SortKey)
+	sk, err := attributevalue.Marshal(f.SortKey)
 	if err != nil {
 		panic(err)
 	}
