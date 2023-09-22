2a1bfb0c8a80b8f15c0f9b26937b1bc168daae04
Jack Goodby <jackgoodby@gmail.com>
2023-09-22 19:47:10 +0100

    new api response keys

--- frontend/src/components/FixturePanel.tsx
+++ frontend/src/components/FixturePanel.tsx
@@ -7,29 +7,31 @@ interface FixturePanelProps {
 }
 
 interface FixtureData {
-  json_id?: string;
-  json_sort_key?: string;
-  json_start_at?: string;
-  json_ended_at?: string;
-  json_court?: string;
-  json_in_progress?: string;
-  json_tournament_name?: string;
+  id?: string;
+  sort_key?: string;
+  scheduled_start_at?: string;
+  actual_start_at?: string;
+  ended_at?: string;
+  court?: string;
+  in_progress?: string;
+  tournament_name?: string;
 }
 
 export default function FixturePanel(props: FixturePanelProps) {
   console.log(props);
   const friendlyTime = formatFriendlyMatchTime(
-    props.fixtureData?.json_start_at,
+    props.fixtureData?.scheduled_start_at,
+    props.fixtureData?.actual_start_at,
   );
   return (
     <div className="fixture">
       <div className="fixture-title-bar">
-        <span className="court">Court {props.fixtureData?.json_court}</span>
+        <span className="court">Court {props.fixtureData?.court}</span>
       </div>
       <div className="fixture-card">
         <div className="fixture-card-wrap">
           <div className="fixture-name">
-            {props.fixtureData?.json_tournament_name}
+            {props.fixtureData?.tournament_name}
           </div>
           <Team teamName="teama" />
           <Team teamName="teamb" />
