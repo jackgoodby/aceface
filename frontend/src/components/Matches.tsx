7767548f407251613f34059f7f12577ce787609b
Jack Goodby <jackgoodby@gmail.com>
2023-09-23 16:30:47 +0100

    scores

--- frontend/src/components/Matches.tsx
+++ frontend/src/components/Matches.tsx
@@ -12,10 +12,10 @@ import fixture8 from "../data/D1-CT7-3";
 export default function Matches() {
   return (
     <div id="fixtures">
+      <FlatMatch matchData={fixture3} />
       <FlatMatch matchData={fixture2} />
       <FlatMatch matchData={fixture5} />
       <FlatMatch matchData={fixture7} />
-      <FlatMatch matchData={fixture3} />
       <FlatMatch matchData={fixture8} />
       <FlatMatch matchData={fixture1} />
       <FlatMatch matchData={fixture4} />
--- frontend/src/data/D1-CT5-2.tsx
+++ frontend/src/data/D1-CT5-2.tsx
@@ -25,7 +25,7 @@ const fixture5: MatchData = {
         seed: 3,
       },
     ],
-    scores: [6, 4],
+    scores: [6],
   },
 };
 
