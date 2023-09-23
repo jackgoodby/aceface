c1df9a9e86659c3015c00844bc4b2ac4d20ff6be
Jack Goodby <jackgoodby@gmail.com>
2023-09-23 14:53:26 +0100

    Added fixture 2

--- frontend/src/components/Matches.tsx
+++ frontend/src/components/Matches.tsx
@@ -1,12 +1,14 @@
 import FlatMatch from "./FlatMatch";
 import React from "react";
 import fixture1 from "../data/D1-CT1-1";
+import fixture2 from "../data/D1-CT1-2";
 import fixture4 from "../data/D1-CT5-1";
 import fixture6 from "../data/D1-CT7-1";
 
 export default function Matches() {
   return (
     <div id="fixtures">
+      <FlatMatch matchData={fixture2} />
       <FlatMatch matchData={fixture1} />
       <FlatMatch matchData={fixture4} />
       <FlatMatch matchData={fixture6} />
---
+++ frontend/src/data/D1-CT1-2.tsx
@@ -0,0 +1,43 @@
+import { MatchData } from "../types/MatchData";
+
+const fixture2: MatchData = {
+  scheduled_start_at: "2023-09-23T14:00:00Z",
+  actual_start_at: "",
+  ended_at: "",
+  court: "1",
+  tournament_name: "Men's Doubles Final",
+  team_a: {
+    id: "teama",
+    players: [
+      {
+        title: "Mr. R.",
+        last_name: "Banno",
+        seed: 2,
+      },
+      {
+        title: "Mr. S.",
+        last_name: "Hathaway",
+        seed: 2,
+      },
+    ],
+    scores: [],
+  },
+  team_b: {
+    id: "teamb",
+    players: [
+      {
+        title: "Mr. A.",
+        last_name: "Mentjes",
+        seed: 1,
+      },
+      {
+        title: "Mr. C.",
+        last_name: "Hill",
+        seed: 1,
+      },
+    ],
+    scores: [],
+  },
+};
+
+export default fixture2;
