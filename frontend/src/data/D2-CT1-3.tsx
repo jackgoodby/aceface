3d34163129f95560bb2176dac947e86274341454
Jack Goodby <jackgoodby@gmail.com>
2023-09-24 17:59:59 +0100

    jhg

--- frontend/src/data/D2-CT1-3.tsx
+++ frontend/src/data/D2-CT1-3.tsx
@@ -20,7 +20,7 @@ const fixture11: MatchData = {
         seed: 3,
       },
     ],
-    scores: [2, 5],
+    scores: [2, 6, 0],
   },
   team_b: {
     id: "teamb",
@@ -36,7 +36,7 @@ const fixture11: MatchData = {
         seed: 7,
       },
     ],
-    scores: [6, 2],
+    scores: [6, 2, 0],
   },
 };
 
