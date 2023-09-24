a3d3e8b27edc48115f00a864f394c4739a06605f
Jack Goodby <jackgoodby@gmail.com>
2023-09-24 14:16:52 +0100

    asd

--- frontend/src/components/Matches.tsx
+++ frontend/src/components/Matches.tsx
@@ -19,11 +19,11 @@ import fixture15 from "../data/D2-CT7-2";
 export default function Matches() {
   return (
     <div id="fixtures">
+      <FlatMatch matchData={fixture10} />
+      <FlatMatch matchData={fixture13} />
       <FlatMatch matchData={fixture9} />
       <FlatMatch matchData={fixture12} />
-      <FlatMatch matchData={fixture10} />
       <div className="fullwidth-title-bar">Later Today</div>
-      <FlatMatch matchData={fixture13} />
       <FlatMatch matchData={fixture14} />
       <FlatMatch matchData={fixture11} />
       <FlatMatch matchData={fixture15} />
