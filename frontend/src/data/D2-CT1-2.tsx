59a94f9991276dccae2fcf482c5fee967d87cf02
Jack Goodby <jackgoodby@gmail.com>
2023-09-24 14:54:05 +0100

    U

--- frontend/src/data/D2-CT1-2.tsx
+++ frontend/src/data/D2-CT1-2.tsx
@@ -20,7 +20,7 @@ const fixture10: MatchData = {
         seed: 2,
       },
     ],
-    scores: [1, 0],
+    scores: [2, 0],
   },
   team_b: {
     id: "teamb",
@@ -36,7 +36,7 @@ const fixture10: MatchData = {
         seed: 3,
       },
     ],
-    scores: [0, 0],
+    scores: [1, 0],
   },
 };
 
